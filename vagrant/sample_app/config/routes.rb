71a79b58e3e523b44b862879842e5b479af37996
Infratop <Infratop@infratopnoMacBook-Air.local>
2018-11-17 13:45:36 +0900

    [Start]3-6

--- vagrant/sample_app/config/routes.rb
+++ vagrant/sample_app/config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   get 'posts/new'
   get '/top' => 'root#top'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   post '/posts' => 'posts#create'
+  get '/posts' => 'posts#index'
 end
