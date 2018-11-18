5e9a5e9ef4d212e63c36636238d05fd3c791bc04
Infratop <Infratop@infratopnoMacBook-Air.local>
2018-11-18 15:41:36 +0900

    [Finish]3-9

--- vagrant/sample_app/app/controllers/posts_controller.rb
+++ vagrant/sample_app/app/controllers/posts_controller.rb
@@ -10,10 +10,10 @@ class PostsController < ApplicationController
       redirect_to post_path(post.id)
     end
     def create
-  	   post = Post.new(post_params)
-  	   post.save
-  	   redirect_to '/top'
-  	end
+      post = Post.new(post_params)
+      post.save
+      redirect_to '/top'
+    end
   	def index
   		@posts = Post.all
   	end
@@ -29,6 +29,9 @@ class PostsController < ApplicationController
         redirect_to post_path(post.id)
     end
     def destroy
+      post = Post.find(params[:id])
+      post.destroy
+      redirect_to posts_path
     end
    private
 
--- vagrant/sample_app/config/initializers/application_controller_renderer.rb
+++ vagrant/sample_app/config/initializers/application_controller_renderer.rb
@@ -5,4 +5,4 @@
 #     http_host: 'example.org',
 #     https: false
 #   )
-# end
+# endRefile.secret_key = 'b7f2e2738595c1b5033ca4ae788107a4f676e7d986a2851c1cec0d7022c9c300f56ed13ac3764fb7484df817091f59e3a83d0e8fbdacb31fc418654fd277996e'
